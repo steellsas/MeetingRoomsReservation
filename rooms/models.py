f6e6d544ffa6923af3b288976f0dc3b20b2f39cd
steellsas <aplienius@gmail.com>
2021-09-29 23:54:48 +0300

    add rooms model, migrate

--- rooms/models.py
+++ rooms/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class MeetingRoom(models.Model):
+    title = models.CharField(max_length=150)
+
+    def __str__(self):
+        return f'Meetings Room title :{self.title}'
