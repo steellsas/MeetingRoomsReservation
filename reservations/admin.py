135cbdffa49534f91530304eb5693c317918be0f
steellsas <aplienius@gmail.com>
2021-10-01 21:50:01 +0300

    add MeetingRoomReservations

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from reservations.models import MeetingRoomReservation, EmployeesInMeetingRoom
 
-# Register your models here.
+
+admin.site.register(EmployeesInMeetingRoom)
+admin.site.register(MeetingRoomReservation)
+
