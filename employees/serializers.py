e27aeb846a5e31992ea30f15e3316169cbd2d964
steellsas <aplienius@gmail.com>
2021-10-06 12:35:31 +0300

    updated emmployee app

--- employees/serializers.py
+++ employees/serializers.py
@@ -10,7 +10,7 @@ class UserSerializer(serializers.ModelSerializer):
         write_only_fields = ('password',)
 
 
-class EmployeeSerializer(serializers.ModelSerializer):
+class EmployeeSerializer1(serializers.ModelSerializer):
     account = UserSerializer()
 
     class Meta:
@@ -27,3 +27,9 @@ class EmployeeSerializer(serializers.ModelSerializer):
         employee_instance = Employee.objects.create(**validated_data, account=user_instance)
         employee_instance.save(0)
         return employee_instance
+
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Employee
+        fields = ('id','first_name', 'last_name', 'account')
