c1e430759af0202e8577ee9e61eac9cee8f07e6a
steellsas <aplienius@gmail.com>
2021-10-09 14:40:45 +0300

    createating with  room

--- reservations/urls.py
+++ reservations/urls.py
@@ -5,13 +5,14 @@ app_name = 'reservations'
 
 urlpatterns = [
     path('reservations/create/', BookingRoomViewSet.as_view({'post': 'create'}), name='create_reservation'),
-    path('reservations/all/', BookingRoomViewSet.as_view({'get': 'list'})),
+    path('reservations/all/', BookingRoomViewSet.as_view({'get': 'list'}), name='reservations-all'),
     path('reservations/edit/<int:pk>',
-         BookingRoomViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'})),
-    path('reservations/room/<int:pk>',
+         BookingRoomViewSet.as_view({'get': 'retrieve', 'put': 'update', 'delete': 'destroy'}), name='reservations-edit'),
+    path('reservations/room/<int:room_id>',
          ReservationsByRoomViewSet.as_view({'get': 'retrieve'}), name='room_reservations'),
-    path('reservations/room/employee/<int:pk>',
+    path('reservations/room/employee/<int:employee_id>',
          RoomReservationsByEmployeeViewSet.as_view({'get': 'retrieve'}), name='employee_in_room_reservations'),
-    path('reservations/cancel/<int:pk>', BookingRoomViewSet.as_view({'get': 'retrieve', 'delete': 'destroy'})),
+    path('reservations/cancel/<int:pk>',
+         BookingRoomViewSet.as_view({'get': 'retrieve', 'delete': 'destroy'}), name='reservations-cancel'),
 
 ]
--- tests/views/tests_reservations.py
+++ tests/views/tests_reservations.py
@@ -1,9 +1,6 @@
 from rest_framework.test import APITestCase
-
 from django.urls import reverse
-from tests.factories.rooms_model_factory import MeetingRoomFactory
-from tests.factories.reservations_model_factory import RoomBookingFactory
-from tests.factories.employee_model_factory import EmployeeFactory, UserFactory
+
 from django.contrib.auth.models import User
 from employees.models import Employee
 from rooms.models import MeetingRoom
@@ -13,16 +10,40 @@ from reservations.models import RoomBooking
 class TestRoomReservationsViews(APITestCase):
 
     def setUp(self):
+
         self.user = User.objects.create_user(username='username', password='Password')
-        self.user_second = User.objects.create_user(username='username1', password='Password1')
+        self.user_2 = User.objects.create_user(username='username2', password='Password2')
+        self.user_3 = User.objects.create_user(username='username3', password='Password3')
+        self.user_4 = User.objects.create_user(username='username4', password='Password4')
+        self.user_5 = User.objects.create_user(username='username5', password='Password5')
+
         self.client.force_authenticate(self.user)
 
         self.room = MeetingRoom.objects.create(title='Casper')
-        self.room2 = MeetingRoomFactory()
-
+        self.room2 = MeetingRoom.objects.create(title='Home')
 
         self.emp1 = Employee.objects.create(first_name='Andrius', last_name='steels', account=self.user)
-        self.emp2 = Employee.objects.create(first_name='Tom', last_name='max', account=self.user_second)
+        self.emp2 = Employee.objects.create(first_name='Tom', last_name='max', account=self.user_2)
+        self.emp3 = Employee.objects.create(first_name='Ben', last_name='Beard', account=self.user_3)
+        self.emp4 = Employee.objects.create(first_name='Dick', last_name='Dikins', account=self.user_4)
+        self.emp5 = Employee.objects.create(first_name='Zero', last_name='one', account=self.user_5)
+
+        self.reservation1 = RoomBooking.objects.create(
+            title='Party Meeting', date_from='2021-11-10',date_to='2021-11-12', room=self.room)
+        self.reservation1.employees.add(self.emp1)
+
+        self.reservation2 = RoomBooking.objects.create(
+            title='Meeting', date_from='2021-10-22', date_to='2021-10-24', room=self.room)
+        self.reservation1.employees.add(self.emp1, self.emp5, self.emp4)
+
+        self.reservation3 = RoomBooking.objects.create(
+            title='Meeting3', date_from='2021-10-28', date_to='2021-10-29', room=self.room2)
+        self.reservation1.employees.add(self.emp1, self.emp5, self.emp4)
+
+        self.reservation4 = RoomBooking.objects.create(
+            title='Meeting4', date_from='2021-11-28', date_to='2021-11-29', room=self.room2)
+        self.reservation1.employees.add(self.emp1, self.emp5, self.emp3)
+
 
         self.reservation_data = {
             'title':'Party Meeting',
@@ -37,12 +58,15 @@ class TestRoomReservationsViews(APITestCase):
     def test_create_reservation(self):
         response = self.client.post(self.url_name,  self.reservation_data, format='json')
         expected = RoomBooking.objects.first()
-
         self.assertEqual(response.status_code, 201)
         self.assertEqual(f'{expected.title}', 'Party Meeting')
 
     def test_room_reservations(self):
-        pass
+        self.url = reverse()
+        test = RoomBooking.objects.filter(room=self.room.id)
+        for i in range(len(test)):
+            print(test[i].room)
+        response = self.client.post(self.url_name, self.reservation_data, format='json')
 
     def test_employee_in_room_reservations(self):
 
@@ -52,20 +76,37 @@ class TestRoomReservationsViews(APITestCase):
     #     self.assertEqual(RoomBooking.objects.filter(room__id=1).count(), 1)
 
     def test_test_employee_crested(self):
-        self.useris = UserFactory.create()
-        self.employee_1 = EmployeeFactory()
-        self.employee_2 = EmployeeFactory()
+
+
+        # self.useris = UserFactory.create()
+        # self.employee_1 = EmployeeFactory()
+        # self.employee_2 = EmployeeFactory()
+        # self.room10 =MeetingRoomFactory()
+        # # print(self.employee_1.id)
+        # print(vars(self.employee_2))
+
+        # test_reservations = RoomBooking.objects.create(
+        #     title='testas', date_from='2021-11-01', date_to='2021-11-02', room=self.room)
+        # test_reservations.employees.add(self.employee_1)
+
         # self.reservation = RoomBookingFactory.create(employees=(1,2,3))
-        self.reservation = RoomBookingFactory()  #employees.set(self.employee_1)
+        # self.reservation = RoomBookingFactory()  #employees.set(self.employee_1)
+        employees_list = Employee.objects.all()
+
+
+        # tt = Employee.objects.get(pk=(self.employee_1.id))
+        # print(tt)
+        # self.reservation.employees.add(Employee.objects.get(pk=(self.employee_1.id)))
+
+
         # self.reservation.employees.set(self.employee_1)
 
-        print(self.employee_1.first_name)
-        print(self.reservation.date_from, self.reservation.room, self.reservation.employees)
+        # # print(self.employee_1.first_name)
+        # print(self.reservation.date_from, self.reservation.room, self.reservation.employees)
 
-    # def test_ten_rooms(self):
-    #     for i in range(10):
-    #         data_room =MeetingRoomFactory()
-    #         print(data_room)
+        # def test_ten_rooms(self):
+        #     for i in range(10):
+        #         data_room =MeetingRoomFactory()
 
         # response = self.client.get(reverse('room-list'))
         # self.assertEqual(response.status_code,200)
