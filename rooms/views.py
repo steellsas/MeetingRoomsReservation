5ce8d8c4405c10c623e211b6fa39a550db3cb2b9
steellsas <aplienius@gmail.com>
2021-10-10 15:22:26 +0300

    add logging to setting .py

--- rooms/views.py
+++ rooms/views.py
@@ -2,9 +2,12 @@ from rest_framework import viewsets
 from rooms.models import MeetingRoom
 from rooms.serializers import MeetRoomSerializer
 from rest_framework.permissions import IsAuthenticated
+import logging,traceback
 
 
 class MeetRoomViewSet(viewsets.ModelViewSet):
     queryset = MeetingRoom.objects.all()
+    logging.info('Actions with meeting rooms')
     serializer_class = MeetRoomSerializer
     permission_classes = [IsAuthenticated]
+
